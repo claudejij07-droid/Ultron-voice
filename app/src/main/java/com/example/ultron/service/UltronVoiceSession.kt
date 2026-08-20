6f84326b6dd0d98e5b698cc8f242e85cd3c0a419
claudejij07-droid <claudejij07@gmail.com>
2026-08-20 14:15:28 +0600

    Wire assist trigger to overlay service

--- app/src/main/java/com/example/ultron/service/UltronVoiceSession.kt
+++ app/src/main/java/com/example/ultron/service/UltronVoiceSession.kt
@@ -1,9 +1,12 @@
 package com.example.ultron.service
 
+import android.content.Intent
 import android.service.voice.VoiceInteractionService
 
 class UltronVoiceSession : VoiceInteractionService() {
     override fun onReady() {
         super.onReady()
+        val overlayIntent = Intent(this, OverlayService::class.java)
+        startService(overlayIntent)
     }
 }
